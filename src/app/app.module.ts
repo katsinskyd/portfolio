11113f7edd99a6a2201431efaa55fdd56f7e736e
Kat Garnier <kathryn.garnier@gmail.com>
2022-06-05 22:21:21 -0400

    made a little site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { GalleryComponent } from './portfolio/gallery/gallery.component';
+import { CommissionsComponent } from './portfolio/commissions/commissions.component';
+import { ConnectComponent } from './connect/connect.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    PortfolioComponent,
+    GalleryComponent,
+    CommissionsComponent,
+    ConnectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTabsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
